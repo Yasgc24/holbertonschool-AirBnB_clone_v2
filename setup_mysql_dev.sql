04bce1990c10cbba2b96b9048e28a5eb8dcf5895
Yasgc24 <4537@holbertonschool.com>
2022-09-06 17:26:17 -0500

    Prepares a MySQL server for the project dev

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,9 @@
+-- Prepares a MySQL server for the project
+-- Create database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- Create user in localhost identified by password
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Grant all privileges to hbnb_dev on the database
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- Grant select privilege to hbnb_dev on the database performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
