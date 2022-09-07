e37366ad90aaf00e73d1555e39b75024210efa5d
Yasgc24 <4537@holbertonschool.com>
2022-09-06 23:12:12 -0500

    Update city, place, review and user

--- models/city.py
+++ models/city.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from turtle import back
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, ForeignKey
@@ -10,3 +11,4 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")
