51176a8f0b4523d819ade9570653176da15121b0
TeoVH <villadamateo64@gmail.com>
2022-09-07 14:09:45 -0500

    Fixed

--- models/place.py
+++ models/place.py
@@ -1,34 +1,42 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from sqlalchemy.sql.schema import Table
-from models.base_model import Base, BaseModel
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
-from sqlalchemy.orm import relationship
-from models.review import Review
 import models
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy.orm import relationship, backref
 
-class Place(BaseModel, Base):
-    """ A place to stay """
-    __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    amenity_ids = []
-    reviews = relationship("Review", backref="state", cascade="delete")
 
-    @property
-    def reviews(self):
-        """returns the list of Review instances with place_id equals to the current Place.id"""
-        instance = models.storage.all(Review)
-        new = []
-        for review in instance.values():
-            if review.place_id == self.id:
-                new.append(review)
-        return new
+class Place(BaseModel, Base if (getenv("HBNB_TYPE_STORAGE")=="db") else object):
+    """ A place to stay"""
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "places"
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+        reviews = relationship(
+            "Review",
+            cascade="all,delete",
+            backref=backref("place", cascade="all,delete"),
+            passive_deletes=True)
+
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
