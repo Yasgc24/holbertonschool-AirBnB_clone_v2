89d7f8f12e3d9a31024e3e5aaed466068dbd7d85
TeoVH <villadamateo64@gmail.com>
2022-09-07 14:07:03 -0500

    Returning to the base mode

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel):
-    """Inherits from BaseModel and Base"""
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    places_amenities = relationship("Place", secondary="place_amenity")
+    name = ""
