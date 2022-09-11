3637ba6e327d8c6c696300b4d056ed940286cbf4
Yasgc24 <4537@holbertonschool.com>
2022-09-11 11:14:29 -0500

    Add task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#  sets up your web servers for the deployment of web_static
+
+# Install Nginx if it not already installed
+sudo apt -y update
+sudo apt -y install nginx
+
+# Create the folders
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+
+# Create a fake HTML file
+    echo "<html>
+    <head>
+    </head>
+    <body>
+        Holberton School
+    </body>
+    </html>" > /data/web_static/releases/test/index.html
+
+# Create a symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of the /data/ folder to the ubuntu user AND group
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update the Nginx configuration to serve the content
+echo sudo sed -i '48i \\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+# Restart Nginx
+sudo service nginx restart
