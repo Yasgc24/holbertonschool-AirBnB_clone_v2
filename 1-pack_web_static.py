83ed00f536cdfa6f6d22c373544b08e83948be19
Yasgc24 <4537@holbertonschool.com>
2022-09-11 11:43:51 -0500

    Add task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""1. Compress before sending"""
+from datetime import datetime
+from os.path import isdir
+from fabric.api import local
+
+def do_pack():
+    """Generates a .tgz archive from the contents"""
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    file = "versions/web_static_{}.tgz".format(date)
+    try:
+        if isdir("versions") is False:
+            local("mkdir versions")
+        local("tar -cvzf {} web_static".format(file))
+        return file
+    except Exception:
+        return None
