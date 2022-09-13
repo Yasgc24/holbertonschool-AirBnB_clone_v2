33037c7f26018c0650138583b4a6c4cd50833b0c
Yasgc24 <4537@holbertonschool.com>
2022-09-13 13:00:40 -0500

    Add task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Display a HTML page"""
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+@app.teardown_appcontext
+def teardown(exc):
+    """"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0")
