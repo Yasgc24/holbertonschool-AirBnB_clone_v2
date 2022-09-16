1c51bc490f480cea1c6115063e789b813f9c216a
Yasgc24 <4537@holbertonschool.com>
2022-09-16 15:00:47 -0500

    Modified task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -2,21 +2,25 @@
 """Starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
-@app.route("//states", strict_slashes=False)
-def states():
+@app.route("/states", strict_slashes=False)
+@app.route("/states7<states_id>", strict_slashes=False)
+def states(state_id=None):
     """Display a HTML page"""
-    states = storage.all("State")
-    return render_template("9-states.html", states=states)
+    states = storage.all(State)
+    if state_id is not None:
+        state_id = State + "." + state_id
+    return render_template("9-states.html", states=states, state_id=state_id)
 
 
 @app.route("/states/id", strict_slashes=False)
 def states_id(id):
     """Display a HTML page"""
-    for state in storage.all("State").values():
+    for state in storage.all(State).values():
         if state.id == id:
             return render_template("9-states.html", states=states)
     return render_template("9-states.html", states=states)
