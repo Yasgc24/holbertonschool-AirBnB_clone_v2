79fbc38f86a21fffa24156f546329b02c85d4b05
Yasgc24 <4537@holbertonschool.com>
2022-11-01 11:00:23 -0500

    Modified task 10

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -2,25 +2,19 @@
 """Starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
-@app.route("/hbnb_filters", strict_slashes=False)
-def states():
-    """Display a HTML page"""
-    states = storage.all("State")
-    return render_template("9-states.html", states=states)
-
-
-@app.route("/states/id", strict_slashes=False)
-def states_id(id):
-    """Display a HTML page"""
-    for state in storage.all("State").values():
-        if state.id == id:
-            return render_template("9-states.html", states=states)
-    return render_template("9-states.html", states=states)
-
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id=None):
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all(State)
+    if state_id is not None:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
 
 @app.teardown_appcontext
 def teardown(exception):
